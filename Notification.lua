47c2a53058ba770dde388908d1632ac812a27987
wisl884 <demoogus@gmail.com>
2025-03-05 18:18:38 +0700

    Update Notification.lua

--- Notification.lua
+++ Notification.lua
@@ -1,1 +1,9 @@
-print ("WelCome To Universal")
+task.spawn(function()
+    wait(1) 
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Welcome To Universal",
+        Text = "Universal script loaded!",
+        Icon = "",
+        Duration = 3
+    })
+end)
