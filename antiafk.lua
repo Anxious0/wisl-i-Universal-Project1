930c25534cb5b1fb43ac1eeabbdab41442f11d2e
_x6ro <demoogus@gmail.com>
2025-09-10 22:47:17 +0700

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,3 +1,5 @@
 RunService.Stepped:Connect(function()
-    lp.Character.HumanoidRootPart.CFrame = lp.Character.HumanoidRootPart.CFrame + Vector3.new(0,0.01,0)
+    if lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
+        lp.Character.HumanoidRootPart.CFrame = lp.Character.HumanoidRootPart.CFrame + Vector3.new(0,0.01,0)
+    end
 end)
